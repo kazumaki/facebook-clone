600ef5ba6bc371f96a87913caeb0a730c3f3db78
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-06-18 13:44:35 -0300

    update db type

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20200203200741) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
     t.integer "author_id"
     t.integer "post_id"
